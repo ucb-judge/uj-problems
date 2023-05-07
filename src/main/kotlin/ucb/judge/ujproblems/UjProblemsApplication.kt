167ce94d9ed1d5395e0e3b89118970e293e9a39a
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-07 00:57:13 -0400

    add: spring boot project with dependencies

---
+++ src/main/kotlin/ucb/judge/ujproblems/UjProblemsApplication.kt
@@ -0,0 +1,11 @@
+package ucb.judge.ujproblems
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjProblemsApplication
+
+fun main(args: Array<String>) {
+	runApplication<UjProblemsApplication>(*args)
+}
